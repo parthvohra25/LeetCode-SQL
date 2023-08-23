d2cebfe4bb8e505bb3fc72548da2cfc20916c0f3
Parth Vohra <72933441+parthvohra25@users.noreply.github.com>
2023-08-23 10:42:46 +0530

    Time: 7 ms (65.42%) | Memory: 14.8 MB (60.71%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,25 +11,50 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 == NULL)
+        //base case
+        if(list1==NULL && list2==NULL){
+            return NULL;
+        }
+        if(list1==NULL){
             return list2;
-
-        if(list2 == NULL)
+        }
+        if(list2==NULL){
             return list1;
+        }
 
-        if(list1 -> val > list2 -> val){
-            swap(list1,list2);
+        //make a dummy node
+        ListNode* dummy=new ListNode();
+        ListNode* temp=dummy;
+        //initalize dummy node with smallest element
+        if(list1->val<list2->val){
+            temp=list1;
+            list1=list1->next;
         }
-        ListNode* res = list1;  // remeber the head of ll
-        while(list1 != NULL && list2 != NULL){
-            ListNode* temp = NULL;
-            while(list1 != NULL && list1 -> val <= list2 -> val){
-                temp = list1;
-                list1 = list1 -> next;
+        else{
+            temp=list2;
+            list2=list2->next;
+        }
+        dummy=temp;
+        //add element into dummy node untill any of the list reached NULL
+        while(list1 && list2){
+            if(list1->val<list2->val){
+            temp->next=list1;
+            list1=list1->next;
             }
-            temp -> next = list2;   // break old connection and build a new
-            swap(list1,list2);
+            else{
+            temp->next=list2;
+            list2=list2->next;
+            }
+            temp=temp->next;
+
         }
-        return res;
+        //left element if in any list
+        if(list1){
+            temp->next=list1;
+        }
+        if(list2){
+            temp->next=list2;
+        }
+        return dummy;
     }
 };
